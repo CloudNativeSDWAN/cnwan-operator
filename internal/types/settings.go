7299d94cde9c7b74bfbb122eafc22ec75a87bc8d
Elis Lulja <elulja@cisco.com>
2021-01-14 09:15:02 +0100

    Include etcd settings in the operator settings (#24)
    The operator yaml settings now have a `serviceRegistry` field that
    contains settings about the service registry to use.
    `etcd` is a new field in the operator's YAML settings that includes a
    list of endpoints of the etcd cluster to connect to.
    Additionally, this commit moves service directory settings from gcloud
    to serviceRegistry to be more consistent and deprecates the old way.
    
    Signed-off-by: Elis Lulja <elulja@cisco.com>

--- internal/types/settings.go
+++ internal/types/settings.go
@@ -1,4 +1,4 @@
-// Copyright © 2020 Cisco
+// Copyright © 2020, 2021 Cisco
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -27,23 +27,29 @@ const (
 	AllowedAnnotations = "service.annotations"
 	// AllowedAnnotationsMap is the key for the allowed annotations map setting
 	AllowedAnnotationsMap = "service.annotationsmap"
+	// ServiceRegistrySettingsKey is the key for the service registry settings
+	ServiceRegistrySettingsKey = "serviceregistry"
+	// DeprecatedGcloudServiceDirectoryKey is the key for the old service
+	// directory. It is currently only used to check if it is there and warn
+	// the user that it is deprecated.
+	DeprecatedGcloudServiceDirectoryKey = "gcloud.servicedirectory"
 )
 
 // Settings of the application
 type Settings struct {
-	Namespace NamespaceSettings `yaml:"namespace"`
-	Gcloud    *GcloudSettings   `yaml:"gcloud"`
+	Namespace                NamespaceSettings `yaml:"namespace"`
+	Service                  ServiceSettings   `yaml:"service"`
+	*ServiceRegistrySettings `yaml:"serviceRegistry"`
+
+	// DEPRECATED: include this under serviceRegistry instead of here.
+	// TODO: remove this on v0.6.0
+	Gcloud *GcloudSettings `yaml:"gcloud"`
 }
 
 // GcloudSettings holds gcloud settings
+// TODO: remove this on v0.6.0
 type GcloudSettings struct {
-	ServiceDirectory *ServiceDirectorySettings `yaml:"serviceDirectory"`
-}
-
-// ServiceDirectorySettings holds settings about gcloud service directory
-type ServiceDirectorySettings struct {
-	DefaultRegion string `yaml:"region"`
-	ProjectName   string `yaml:"project"`
+	ServiceDirectory *DeprecatedServiceDirectorySettings `yaml:"serviceDirectory"`
 }
 
 // ListPolicy is the list type that must be adopted by the operator
@@ -69,4 +75,63 @@ const (
 // NamespaceSettings includes settings about namespaces
 type NamespaceSettings struct {
 	ListPolicy ListPolicy `yaml:"listPolicy"`
+}
+
+// ServiceSettings includes settings about services
+type ServiceSettings struct {
+	Annotations []string `yaml:"annotations"`
+}
+
+// ServiceRegistrySettings contains information about the service registry
+// that must be used, i.e. etcd or service directory.
+type ServiceRegistrySettings struct {
+	*ServiceDirectorySettings `yaml:"gcpServiceDirectory"`
+	*EtcdSettings             `yaml:"etcd"`
+}
+
+// DeprecatedServiceDirectorySettings holds settings about gcloud service directory
+// TODO: remove this on v0.6.0
+type DeprecatedServiceDirectorySettings struct {
+	DefaultRegion string `yaml:"region"`
+	ProjectName   string `yaml:"project"`
+}
+
+// ServiceDirectorySettings holds settings about gcloud service directory
+type ServiceDirectorySettings struct {
+	// DefaultRegion is the default region where objects will be registered to
+	// in case the region is not mentioned explicitly.
+	DefaultRegion string `yaml:"defaultRegion"`
+	// ProjectID is the ID of the gcp project as it appears on google cloud
+	// console.
+	ProjectID string `yaml:"projectID"`
+}
+
+// EtcdAuthenticationType specifies how the cnwan operator must authenticate to
+// the etcd cluster
+type EtcdAuthenticationType string
+
+const (
+	// EtcdAuthWithNothing specifies that no authentication must be
+	// performed.
+	EtcdAuthWithNothing EtcdAuthenticationType = ""
+	// EtcdAuthWithUsernamePassw specifies that authentication needs to be done
+	// with username and password.
+	EtcdAuthWithUsernamePassw EtcdAuthenticationType = "WithUsernameAndPassword"
+	// EtcdAuthWithTLS specifies that authentication must be done with TLS.
+	EtcdAuthWithTLS EtcdAuthenticationType = "WithTLS"
+)
+
+// EtcdSettings holds settings about etcd
+type EtcdSettings struct {
+	Authentication EtcdAuthenticationType `yaml:"authentication,omitempty"`
+	Prefix         *string                `yaml:"prefix,omitempty"`
+	Endpoints      []*EtcdEndpoint        `yaml:"endpoints"`
+}
+
+// EtcdEndpoint specifies an endpoint where to connect to.
+// The port is a pointer, because if it is not specifies the well-known etcd
+// port assigned from IANA will be used instead.
+type EtcdEndpoint struct {
+	Host string `yaml:"host"`
+	Port *int   `yaml:"port"`
 }
