e64e6b0521f7c46ab6b99d4370448df65ab19e37
Elis Lulja <elulja@cisco.com>
2023-03-22 14:01:39 +0100

    refactor(ctrl-utils): improve service checking
    Signed-off-by: Elis Lulja <elulja@cisco.com>

--- controllers/utils.go
+++ controllers/utils.go
@@ -19,10 +19,13 @@
 package controllers
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"net"
 	"strings"
 
+	serego "github.com/CloudNativeSDWAN/serego/api/core/types"
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -68,7 +71,38 @@ func filterAnnotations(currentAnnotations map[string]string, filter []string) ma
 	return filtered
 }
 
-func shouldWatchLabel(labels map[string]string, watchAllByDefault bool) bool {
+func getIPsFromService(service *corev1.Service) ([]string, error) {
+	ipsMap := map[string]bool{}
+	for _, externalIP := range service.Spec.ExternalIPs {
+		ipsMap[externalIP] = true
+	}
+
+	// Get data from load balancers
+	for _, ing := range service.Status.LoadBalancer.Ingress {
+		if ing.IP != "" {
+			ipsMap[ing.IP] = true
+		}
+
+		if ing.Hostname != "" {
+			resolvedIPs, err := net.LookupHost(ing.Hostname)
+			if err != nil {
+				return nil, err
+			}
+
+			for _, resolvedIP := range resolvedIPs {
+				ipsMap[resolvedIP] = true
+			}
+		}
+	}
+
+	ips := []string{}
+	for ip := range ipsMap {
+		ips = append(ips, ip)
+	}
+	return ips, nil
+}
+
+func checkNsLabels(labels map[string]string, watchAllByDefault bool) bool {
 	switch labels[watchLabel] {
 	case watchEnabledLabel:
 		return true
@@ -79,25 +113,70 @@ func shouldWatchLabel(labels map[string]string, watchAllByDefault bool) bool {
 	}
 }
 
-func getIPsFromService(service *corev1.Service) ([]string, error) {
-	ips := []string{}
-	ips = append(ips, service.Spec.ExternalIPs...)
+type checkServiceResult struct {
+	passed      bool
+	reason      string
+	err         error
+	annotations map[string]string
+	ips         []string
+	endpoints   []*serego.Endpoint
+}
 
-	// Get data from load balancers
-	for _, ing := range service.Status.LoadBalancer.Ingress {
-		if ing.IP != "" {
-			ips = append(ips, ing.IP)
-		}
-
-		if ing.Hostname != "" {
-			if resolvedIPs, err := net.LookupHost(ing.Hostname); err != nil {
-				return nil, err
-			} else {
-				ips = append(ips, resolvedIPs...)
-			}
-		}
-		ips = append(ips, ing.IP)
+func checkService(service *corev1.Service, annotationsToKeep []string) (result checkServiceResult) {
+	if service.Spec.Type != corev1.ServiceTypeLoadBalancer {
+		result.reason = "not a LoadBalancer"
+		return
 	}
 
-	return ips, nil
+	annotations := filterAnnotations(service.Annotations, annotationsToKeep)
+	if len(annotations) == 0 {
+		result.reason = "no valid annotations"
+		return
+	}
+
+	ips, err := getIPsFromService(service)
+	if len(ips) == 0 {
+		result.reason = "no valid hostnames/ips found"
+		if err != nil {
+			result.err = err
+		}
+
+		return
+	}
+
+	result = checkServiceResult{
+		passed:      true,
+		annotations: annotations,
+		ips:         ips,
+		endpoints:   []*serego.Endpoint{},
+	}
+	for _, port := range service.Spec.Ports {
+		for _, ip := range ips {
+
+			// Create an hashed name for this
+			toBeHashed := fmt.Sprintf("%s:%d", ip, port.Port)
+			h := sha256.New()
+			h.Write([]byte(toBeHashed))
+			hash := hex.EncodeToString(h.Sum(nil))
+
+			result.endpoints = append(result.endpoints, &serego.Endpoint{
+				Namespace: service.Namespace,
+				Service:   service.Name,
+				Name:      fmt.Sprintf("%s-%s", service.Name, hash[:10]),
+				Address:   ip,
+				Port:      port.Port,
+				Metadata:  annotations,
+			})
+		}
+	}
+
+	return
+}
+
+func getEndpointsMapFromSlice(endpoints []*serego.Endpoint) map[string]*serego.Endpoint {
+	epMap := map[string]*serego.Endpoint{}
+	for _, ep := range endpoints {
+		epMap[ep.Name] = ep
+	}
+	return epMap
 }
